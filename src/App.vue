e08fe6ac409e49a5a6596fc1a2e603cf35f1d0a2
potatolee <dean19990226@gmail.com>
2023-02-13 21:51:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <router-link to="/">home</router-link>
+  <router-link to="/about">about</router-link>
+  <RouterView />
+</template>
+<style lang="scss">
+@import "./assets/all.scss";
+</style>
